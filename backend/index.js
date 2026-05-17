f3c61dc1acd107519acc0f7c21ac6b2a7c5dac3b
yuvikapundir2605-lgtm <yuvikapundir2605@gmail.com>
2026-05-17 19:51:09 +0530

    Setup

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
+
+const app= express();
+ 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions={
+    origin:'http//localhost:5173',
+    Credentials:true
+}  
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+}) 
